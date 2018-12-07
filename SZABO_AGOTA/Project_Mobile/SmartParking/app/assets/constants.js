9ff3d41251d3395473b4930d711ba7d84119e45d
Agota Szabo <agota95@gmail.com>
2018-12-07 14:57:36 +0200

    Styled the homePage

---
+++ SZABO_AGOTA/Project_Mobile/SmartParking/app/assets/constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HomePage: {
+      Title: 'SMART PARKING',
+      Text1: '      Hello, you opened the smart parking application. The revolutionary solution wich eases your way of parking.',
+      Text2: '  The only thing yo have to do is to naviate to the Map Page. There you can verify if there are any free spaces in the paring lot of your chose. If the parking space is green, then you are free to go.',
+    },
+  };
--- SZABO_AGOTA/Project_Mobile/SmartParking/app/pages/HomePage.js
+++ SZABO_AGOTA/Project_Mobile/SmartParking/app/pages/HomePage.js
@@ -1,20 +1,25 @@
 // @flow
 import React, { Component } from "react";
-import { View, ScrollView, Text } from "react-native";
+import { View, ScrollView, Text, Image } from "react-native";
 import {General }from "../styles/Colors";
+import data from "../assets/constants";
 
 export default class HomePage extends Component {
-
+  constructor(props) {
+    super(props);
+  }
+  
   render() {
     return (
       <View style={styles.container}>
       
       <ScrollView>
-        <Text style={styles.text}> HOME PAGE </Text>
-        <Text style={styles.text}> HOME PAGE </Text>
-        <Text style={styles.text}> HOME PAGE </Text>
-        <Text style={styles.text}> HOME PAGE </Text>
-      </ScrollView>
+        <Text style={styles.textTitle}> {data.HomePage.Title} </Text>
+        <Text style={styles.textDescription}>{data.HomePage.Text1}</Text>
+        <Text style={styles.textDescription}>{data.HomePage.Text2}</Text>
+        <Image source={{uri: 'https://media.giphy.com/media/3oKGzApIBl3UyNo7Cw/giphy.gif'}} style={{width: 300, height:300, alignSelf: "center" }} />
+        <Text style={styles.textDescription}> HOME PAGE </Text>
+        </ScrollView>
       </View>
     )
   }
@@ -25,11 +30,20 @@ const styles = {
     flex:1,
     backgroundColor: General.white
   },
-  text: {
-    fontSize: 50,
+  textTitle: {
+    fontSize: 35,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginTop: 300,
+    marginTop: 30,
+    color: General.oxford_blue
+  },
+  textDescription: {
+    fontSize: 15,
+    fontWeight: 'normal',
+    textAlign: 'center',
+    marginTop: 30,
+    marginRight: 15,
+    marginLeft: 15,
     color: General.oxford_blue
   }
 }
