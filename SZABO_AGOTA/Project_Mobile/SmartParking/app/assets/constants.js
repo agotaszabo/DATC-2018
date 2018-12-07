ec21532f87eb22a9bc653ae7105430e6bea931c9
Agota Szabo <agota95@gmail.com>
2018-12-07 15:04:06 +0200

    Test commit.

--- SZABO_AGOTA/Project_Mobile/SmartParking/app/assets/constants.js
+++ SZABO_AGOTA/Project_Mobile/SmartParking/app/assets/constants.js
@@ -3,5 +3,6 @@ module.exports = {
       Title: 'SMART PARKING',
       Text1: '      Hello, you opened the smart parking application. The revolutionary solution wich eases your way of parking.',
       Text2: '  The only thing yo have to do is to naviate to the Map Page. There you can verify if there are any free spaces in the paring lot of your chose. If the parking space is green, then you are free to go.',
+      Text3: 'sdfsdf',
     },
   };
