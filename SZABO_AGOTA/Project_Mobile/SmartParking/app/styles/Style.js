b6288155e6dc1b282cf04cb02749db7f08da321c
Agota Szabo <agota95@gmail.com>
2019-01-05 23:20:07 +0200

    Added new styles in Style.js

--- SZABO_AGOTA/Project_Mobile/SmartParking/app/styles/Style.js
+++ SZABO_AGOTA/Project_Mobile/SmartParking/app/styles/Style.js
@@ -1,12 +1,30 @@
+import {General }from "../styles/Colors";
+
 let GeneralStyle = {
     pageStyle: {
      //ad some style here
      marginHorizontal:12
     },
+    textTitle: {
+      fontSize: 35,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      marginTop: 30,
+      color: General.oxford_blue
+    },
+    textDescription: {
+      fontSize: 15,
+      fontWeight: 'normal',
+      textAlign: 'center',
+      marginTop: 30,
+      marginRight: 15,
+      marginLeft: 15,
+      color: General.oxford_blue
+    }
   };
   
   
-  module.exports = {
+  export {
     GeneralStyle
   };
   
