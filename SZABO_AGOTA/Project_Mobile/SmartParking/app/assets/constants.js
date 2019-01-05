94cd9cb891f571f9554370aaf50815504f61c0b3
Agota Szabo <agota95@gmail.com>
2019-01-05 23:42:59 +0200

    Added constants for the 13 paring lots.

--- SZABO_AGOTA/Project_Mobile/SmartParking/app/assets/constants.js
+++ SZABO_AGOTA/Project_Mobile/SmartParking/app/assets/constants.js
@@ -5,4 +5,45 @@ module.exports = {
       Text2: '  The only thing yo have to do is to naviate to the Map Page. There you can verify if there are any free spaces in the paring lot of your chose. If the parking space is green, then you are free to go.',
       Text3: 'sdfsdf',
     },
+
+    MapPageCoordinates: {
+      P1_lat: 45.753747,
+      P1_long: 21.227239,
+
+      P2_lat: 45.753755,
+      P2_long: 21.227120,
+
+      P3_lat: 45.753769,
+      P3_long: 21.226998,
+
+      P4_lat: 45.753786,
+      P4_long: 21.226858,
+
+      P5_lat: 45.753800,
+      P5_long: 21.226721,
+      
+      P6_lat: 45.753815,
+      P6_long: 21.226565,
+      
+      P7_lat: 45.753814,
+      P7_long: 21.226430,
+      
+      P8_lat: 45.753728,
+      P8_long: 21.226466,
+
+      P9_lat: 45.753704,
+      P9_long: 21.226622,
+
+      P10_lat: 45.753687, 
+      P10_long:  21.226739,
+
+      P11_lat: 45.753680,  
+      P11_long: 21.226884,
+
+      P12_lat: 45.753654, 
+      P12_long: 21.227026,
+
+      P13_lat: 45.753645, 
+      P13_long: 21.227174
+    }
   };
