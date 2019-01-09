f3fbc55f99acda60760b6676b3410d54e46e3189
Agota Szabo <agota95@gmail.com>
2019-01-09 23:44:00 +0200

    Update: RootReducer.

--- SZABO_AGOTA/Project_Mobile/SmartParking/common/redux/reducers/RootReducer.js
+++ SZABO_AGOTA/Project_Mobile/SmartParking/common/redux/reducers/RootReducer.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
 import {appReducer} from "./appReducer";
+// import {mapReducer} from "./mapReducer"
 //import reducers
+
 export const RootReducer = combineReducers({
-    appReducer: appReducer
-//add reducers
+    appReducer: appReducer,
+    // mapReducer: mapReducer
+    //add reducers
 })
