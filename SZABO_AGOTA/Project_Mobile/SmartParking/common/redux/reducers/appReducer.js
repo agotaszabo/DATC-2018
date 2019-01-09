01cfd03cb11195b3828f28e5832256734cac3287
Agota Szabo <agota95@gmail.com>
2019-01-09 23:41:15 +0200

    Update: AppReducer.

--- SZABO_AGOTA/Project_Mobile/SmartParking/common/redux/reducers/appReducer.js
+++ SZABO_AGOTA/Project_Mobile/SmartParking/common/redux/reducers/appReducer.js
@@ -1,19 +1,29 @@
-let intialState = () => {
-    isLoading: false
-}
+import { GET_ALL_DATA_REQUEST, GET_ALL_DATA_SUCCESS, GET_ALL_DATA_ERROR } from '../actions/actionTypes'
 
-let appActionTypes = {
-    isLoading: "appIsLoading"
+let initialState = {
+    isLoading: false,
+    data: null
 }
 
 export const appReducer =
-    function (state = {}, action) {
+    function (state = initialState, action) {
         switch (action.type) {
-            case appActionTypes.isLoading:
+            case GET_ALL_DATA_REQUEST: {
+                return {
+                    ...state
+                }
+            }
+            case GET_ALL_DATA_SUCCESS: {
                 return {
                     ...state,
-                    ...action
+                    data: action.payload
                 }
+            }
+            case GET_ALL_DATA_ERROR: {
+                return {
+                    ...state
+                }
+            }
         }
         return state;
     }
